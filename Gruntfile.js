80ad7e56dc0bb05e7c2ecfa848ec40a1b0b3bd88
Sly <khmedj@gmail.com>
2020-09-29 21:00:52 +0100

    Changed colors to dracula palettes and updated settings and grunt options to use node-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,32 @@
-module.exports = function(grunt) {
-
+const sass = require("node-sass");
+module.exports = function (grunt) {
   grunt.initConfig({
     watch: {
       css: {
-        files: ['src/**/*.scss'],
-        tasks: ['sass'],
+        files: ["src/**/*.scss"],
+        tasks: ["sass"],
         options: {
           spawn: false,
         },
-      }
+      },
     },
 
     sass: {
       dist: {
         options: {
-         style: 'expanded'
-       },
+          implementation: sass,
+          style: "expanded",
+        },
         files: {
-          'dist/dist.css': 'src/main.scss'
-        }
-      }
+          "dist/dist.css": "src/main.scss",
+        },
+      },
     },
   });
 
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks("grunt-newer");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-sass");
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask("default", ["sass"]);
 };
